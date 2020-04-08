05e50622a8eb5eb8b5fd81f5b74cd79294f93a90
Toshiya Kawasaki <toshiya@abeja.asia>
2020-04-08 16:06:54 +0900

    Lint

--- abeja/version.py
+++ abeja/version.py
@@ -1,11 +1,14 @@
 import tomlkit
 import os
+
+
 def _get_project_meta():
     toml_path = os.path.join(os.path.dirname(__file__), '../pyproject.toml')
     with open(toml_path) as pyproject:
         file_contents = pyproject.read()
 
     return tomlkit.parse(file_contents)['tool']['poetry']
+
 
 pkg_meta = _get_project_meta()
 VERSION = pkg_meta['version']
