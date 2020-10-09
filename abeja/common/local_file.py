cbcd94d879ee90fbe58afb3f7a6742673da8d8aa
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2020-10-09 12:21:15 +0900

    Fix stale file handle

--- abeja/common/local_file.py
+++ abeja/common/local_file.py
@@ -11,6 +11,7 @@ local file is saved in MOUNT_DIR and follows the rules below.
 
 """
 import os
+import os.path
 from functools import wraps
 from urllib.parse import urlparse
 from datetime import datetime
@@ -22,6 +23,9 @@ from abeja.common.config import MOUNT_DIR, DEFAULT_CHUNK_SIZE
 # often sets random seed, it causes file name conflict. So we have our own
 # random generator.
 RANDOM = random.Random()
+
+# errno for "Stale file handle". Python's errno module doesn't define the value.
+E_STALE_FILE_HANDLE = 116
 
 
 def use_binary_cache(func):
@@ -147,7 +151,13 @@ def _read_file(path, file_type):
     if file_type == 'binary':
         mode += 'b'
     with open(path, mode) as f:
-        return f.read()
+        try:
+            return f.read()
+        except OSError as exc:
+            # The file is already deleted in the NFS server (EFS), try to re-open it and read.
+            if exc.errno == E_STALE_FILE_HANDLE:
+                with open(path, mode) as f2:
+                    return f2.read()
 
 
 def _read_iter_content_file(path, chunk_size):
@@ -182,7 +192,14 @@ def _write_iter_file(path, file_type, iter_content):
     mode = 'w'
     if file_type == 'binary':
         mode += 'b'
+
+    # Write cache contents to a temporary file, periodically check whether an other
+    # process has written the cache file or not. If so, this process will stop downloading
+    # contents as soon as possible and remove a temporary file.
     with open(tmppath, mode) as f:
         for content in iter_content:
             f.write(content)
+            if os.path.exists(path):
+                os.remove(tmppath)
+                return
     os.replace(tmppath, path)
