506021ee9a5be7a2698da49b978717ce2b9ea1b9
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2020-10-10 10:42:52 +0900

    test: use_iter_content_cache

--- tests/common/test_local_file.py
+++ tests/common/test_local_file.py
@@ -1,6 +1,9 @@
 from abeja.common import local_file
-from abeja.common.local_file import use_text_cache, use_binary_cache
+from abeja.common.local_file import use_text_cache, use_binary_cache, use_iter_content_cache
+from abeja.common.config import DEFAULT_CHUNK_SIZE
 import pytest
+import secrets
+from functools import partial
 
 
 class SourceURI:
@@ -15,8 +18,8 @@ def mount_dir(monkeypatch, tmpdir):
 
 
 @pytest.fixture
-def read_cache_factory(mount_dir, tmp_path):
-    def factory(cache_func, content):
+def fake_file_factory(mount_dir, tmp_path):
+    def factory(content):
         filename = 'testfile'
         obj = SourceURI(f'http://example.com/files/{filename}')
         original = tmp_path / filename
@@ -28,21 +31,51 @@ def read_cache_factory(mount_dir, tmp_path):
             mode = 'rb'
             original.write_bytes(content)
 
-        with open(str(original), mode) as f:
-            decorated = cache_func(f.read)
-            return decorated(obj)
+        return open(str(original), mode), obj
     return factory
 
 
-def test_use_text_cache(read_cache_factory):
+@pytest.fixture
+def read_file_factory(fake_file_factory):
+    def factory(cache_func, content):
+        f, obj = fake_file_factory(content)
+
+        with f:
+            decorated = cache_func(f.read)
+            return decorated(obj), decorated(obj)
+    return factory
+
+
+@pytest.fixture
+def read_iter_factory(fake_file_factory):
+    def factory(cache_func, content):
+        f, obj = fake_file_factory(content)
+
+        def make_iter(chunk_size):
+            return iter(partial(f.read, chunk_size), b'')
+
+        with f:
+            decorated = cache_func(make_iter)
+            return decorated(obj), decorated(obj)
+    return factory
+
+
+def test_use_text_cache(read_file_factory):
     content = 'Hello, World!'
-    assert read_cache_factory(use_text_cache, content) == content
-    # Read from cache
-    assert read_cache_factory(use_text_cache, content) == content
+    saved, cached = read_file_factory(use_text_cache, content)
+    assert saved == content
+    assert cached == content
 
 
-def test_use_binary_cache(read_cache_factory):
+def test_use_binary_cache(read_file_factory):
     content = b'test'
-    assert read_cache_factory(use_binary_cache, content) == content
-    # Read from cache
-    assert read_cache_factory(use_binary_cache, content) == content
+    saved, cached = read_file_factory(use_binary_cache, content)
+    assert saved == content
+    assert cached == content
+
+
+def test_use_iter_content_cache(read_iter_factory):
+    content = secrets.token_bytes(int(DEFAULT_CHUNK_SIZE * 3.7))
+    saved, cached = read_iter_factory(use_iter_content_cache, content)
+    assert b''.join(list(saved)) == content
+    assert b''.join(list(cached)) == content
