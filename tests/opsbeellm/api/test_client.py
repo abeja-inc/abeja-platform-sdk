a95810462fa9529ef744dd21645189ab9dfbd253
Yagami360 <y034112@gmail.com>
2023-12-14 06:01:18 +0000

    fix OpsBeeLLM SDK with create history

--- tests/opsbeellm/api/test_client.py
+++ tests/opsbeellm/api/test_client.py
@@ -14,6 +14,49 @@ INPUT_TEXT = 'ABEJAについて教えて'
 OUTPUT_TEXT = 'ABEJAは、スペイン語で「ミツバチ」の意味であり、植物の受粉を手伝い、世界の食料生産を支える存在として社名になっています。'
 INPUT_TOKEN_COUNT = 10
 OUTPUT_TOKEN_COUNT = 10
+
+DEPLOYMENT_QA_RES = {
+    'id': DEPLOYMENT_ID,
+    'account_id': ACCOUNT_ID,
+    'organization_id': ORGANIZATION_ID,
+    'name': 'deployment name',
+    'description': 'deployment description',
+    'type': 'qa',
+    'history_count': 0,
+    'created_at': "2023-12-14T04:42:34.913644Z",
+    'updated_at': "2023-12-14T04:42:34.913644Z",
+}
+DEPLOYMENT_CHAT_RES = {
+    'id': DEPLOYMENT_ID,
+    'account_id': ACCOUNT_ID,
+    'organization_id': ORGANIZATION_ID,
+    'name': 'deployment name',
+    'description': 'deployment description',
+    'type': 'chat',
+    'history_count': 0,
+    'created_at': "2023-12-14T04:42:34.913644Z",
+    'updated_at': "2023-12-14T04:42:34.913644Z",
+}
+THREADS_RES = {
+    'account_id': ACCOUNT_ID,
+    'organization_id': ORGANIZATION_ID,
+    'deployment_id': DEPLOYMENT_ID,
+    'threads': [
+        {
+            'id': THREAD_ID,
+            'account_id': ACCOUNT_ID,
+            'organization_id': ORGANIZATION_ID,
+            'deployment_id': DEPLOYMENT_ID,
+            'name': "thread name",
+            'description': "thread description",
+            'created_at': "2023-12-14T04:42:34.913644Z",
+            'updated_at': "2023-12-14T04:42:34.913644Z",
+        }
+    ],
+    'offset': 0,
+    'limit': 1000,
+    'has_next': False,
+}
 HISOTRY_RES = {
     "id": HISTORY_ID,
     "account_id": ACCOUNT_ID,
@@ -35,6 +78,23 @@ class TestOpsBeeLLMAPIClient(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test_create_history(self, m):
+        # get-deployment-api mock
+        path = '/accounts/{}/organizations/{}/deployments/{}'.format(
+            ACCOUNT_ID,
+            ORGANIZATION_ID,
+            DEPLOYMENT_ID,
+        )
+        m.get(path, json=DEPLOYMENT_QA_RES)
+
+        # get-threads-api mock
+        path = '/accounts/{}/organizations/{}/deployments/{}/threads'.format(
+            ACCOUNT_ID,
+            ORGANIZATION_ID,
+            DEPLOYMENT_ID,
+        )
+        m.get(path, json=THREADS_RES)
+
+        # create-history-api mock
         path = '/accounts/{}/organizations/{}/deployments/{}/threads/{}/history'.format(
             ACCOUNT_ID,
             ORGANIZATION_ID,
@@ -43,8 +103,71 @@ class TestOpsBeeLLMAPIClient(unittest.TestCase):
         )
         m.post(path, json=HISOTRY_RES)
 
+        # unit test
         client = APIClient()
         ret = client.create_history(
+            ACCOUNT_ID,
+            ORGANIZATION_ID,
+            DEPLOYMENT_ID,
+            INPUT_TEXT,
+            OUTPUT_TEXT,
+            INPUT_TOKEN_COUNT,
+            OUTPUT_TOKEN_COUNT,
+        )
+        expected_payload = {
+            'input_text': INPUT_TEXT,
+            'output_text': OUTPUT_TEXT,
+            'input_token_count': INPUT_TOKEN_COUNT,
+            'output_token_count': OUTPUT_TOKEN_COUNT,
+            'tag_ids': [],
+            'metadata': [],
+        }
+
+        self.assertDictEqual(m.request_history[2].json(), expected_payload)
+        self.assertDictEqual(ret, HISOTRY_RES)
+
+        with self.assertRaises(BadRequest) as e:
+            client.create_history(
+                ACCOUNT_ID,
+                ORGANIZATION_ID,
+                DEPLOYMENT_ID,
+                input_text=None,
+                output_text=None,
+            )
+        self.assertEqual(e.exception.error_description, '"input_text" is necessary')
+
+        with self.assertRaises(BadRequest) as e:
+            client.create_history(
+                ACCOUNT_ID,
+                ORGANIZATION_ID,
+                DEPLOYMENT_ID,
+                input_text=INPUT_TEXT,
+                output_text=None,
+            )
+        self.assertEqual(e.exception.error_description, '"output_text" is necessary')
+
+    @requests_mock.Mocker()
+    def test_create_chat_history(self, m):
+        # get-deployment-api mock
+        path = '/accounts/{}/organizations/{}/deployments/{}'.format(
+            ACCOUNT_ID,
+            ORGANIZATION_ID,
+            DEPLOYMENT_ID,
+        )
+        m.get(path, json=DEPLOYMENT_CHAT_RES)
+
+        # create-history-api mock
+        path = '/accounts/{}/organizations/{}/deployments/{}/threads/{}/history'.format(
+            ACCOUNT_ID,
+            ORGANIZATION_ID,
+            DEPLOYMENT_ID,
+            THREAD_ID,
+        )
+        m.post(path, json=HISOTRY_RES)
+
+        # unit test
+        client = APIClient()
+        ret = client.create_chat_history(
             ACCOUNT_ID,
             ORGANIZATION_ID,
             DEPLOYMENT_ID,
@@ -63,11 +186,11 @@ class TestOpsBeeLLMAPIClient(unittest.TestCase):
             'metadata': [],
         }
 
-        self.assertDictEqual(m.request_history[0].json(), expected_payload)
+        self.assertDictEqual(m.request_history[1].json(), expected_payload)
         self.assertDictEqual(ret, HISOTRY_RES)
 
         with self.assertRaises(BadRequest) as e:
-            client.create_history(
+            client.create_chat_history(
                 ACCOUNT_ID,
                 ORGANIZATION_ID,
                 DEPLOYMENT_ID,
@@ -78,7 +201,7 @@ class TestOpsBeeLLMAPIClient(unittest.TestCase):
         self.assertEqual(e.exception.error_description, '"input_text" is necessary')
 
         with self.assertRaises(BadRequest) as e:
-            client.create_history(
+            client.create_chat_history(
                 ACCOUNT_ID,
                 ORGANIZATION_ID,
                 DEPLOYMENT_ID,
